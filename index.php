6a8b7ec57a5a329aaea957330f6e9931481f9681
0x <42121860+x00x0x00x@users.noreply.github.com>
2020-11-08 23:52:15 +0100

    .

---
+++ index.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Nyzo.net</title>
+    <!-- COMMON TAGS -->
+<meta charset="utf-8">
+<title>Nyzo.net</title>
+<!-- Search Engine -->
+<meta name="description" content="Proof-of-diversity blockchain. Ecologically responsible. Scalable. Fast.">
+<meta name="image" content="https://pbs.twimg.com/profile_banners/1053664540712427521/1550912094/1500x500">
+<!-- Schema.org for Google -->
+<meta itemprop="name" content="Nyzo.net">
+<meta itemprop="description" content="Proof-of-diversity blockchain. Ecologically responsible. Scalable. Fast.">
+<meta itemprop="image" content="https://pbs.twimg.com/profile_banners/1053664540712427521/1550912094/1500x500">
+<!-- Twitter -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="Nyzo.net">
+<meta name="twitter:description" content="Proof-of-diversity blockchain. Ecologically responsible. Scalable. Fast.">
+<meta name="twitter:site" content="@nyzo_currency">
+<meta name="twitter:creator" content="@nyzo_currency">
+<meta name="twitter:image:src" content="https://pbs.twimg.com/profile_banners/1053664540712427521/1550912094/1500x500">
+<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+<meta name="og:title" content="Nyzo.net">
+<meta name="og:description" content="Proof-of-diversity blockchain. Ecologically responsible. Scalable. Fast.">
+<meta name="og:image" content="https://pbs.twimg.com/profile_banners/1053664540712427521/1550912094/1500x500">
+<meta name="og:url" content="https://nyzo.net">
+<meta name="og:site_name" content="Nyzo.net">
+<meta name="og:type" content="website">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Staatliches&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+    <style media="screen">
+html {
+position: relative;
+min-height: 100%;
+}
+body {
+margin-bottom: 60px; /* Margin bottom by footer height */
+font-family: 'Staatliches';
+}
+.footer {
+position: absolute;
+bottom: 0;
+width: 100%;
+height: 60px; /* Set the fixed height of the footer here */
+line-height: 60px; /* Vertically center the text there */
+background-color: #f5f5f5;
+}
+
+.container {
+width: auto;
+max-width: 680px;
+padding: 0 15px;
+}
+
+ul {
+  list-style-type: none;
+  padding-inline-start: 0px;
+}
+    </style>
+  </head>
+
+  <body>
+    <main role="main" class="container">
+      <h1 class="mt-5">NYZO.NET</h1>
+      <p class="lead">PROOF-OF-DIVERSITY blockchain.<br>Ecologically responsible.<br>Scalable.<br>Fast.<br><br>
+      <a href="stickers">Get stickers</a></p>
+      <ul class="pb-2">
+        <li>DEVELOPERS<li>
+        <li><i class="fas fa-project-diagram mr-1"></i> <a href="https://nyzo.co/whitePaper" target="_blank">EN whitepaper</a></li>
+        <li><i class="fas fa-cogs mr-1"></i> <a href="https://nyzo.co/mesh" target="_blank">Mesh and explorer</a></li>
+        <li><i class="fas fa-cogs mr-1"></i> <a href="https://explorer.nyzo.net" target="_blank">Explorer (web-only)</a></li>
+        <li><i class="far fa-smile-beam mr-1"></i> <a href="https://nyzo.co/new" target="_blank">New releases</a></li>
+        <li><i class="fas fa-th mr-1"></i> <a href="https://nyzo.co/blockFiles" target="_blank">Block files</a></li>
+        <li><i class="fas fa-search mr-1"></i> <a href="https://nyzo.co/block" target="_blank">Block explorer</a></li>
+        <li><i class="fab fa-github mr-1"></i> <a href="https://github.com/n-y-z-o/nyzoVerifier" target="_blank">Github repository</a></li>
+        <li><i class="fas fa-wallet mr-1"></i> <a href="https://nyzo.co/wallet" target="_blank">Nyzo Web wallet</a></li>
+        <li><i class="fas fa-wallet mr-1"></i> <a href="https://play.google.com/store/apps/details?id=io.threedots.nyzo_wallet" target="_blank"><i>[NEW] Android Mobile wallet</i></a></li>
+        <li><hr>COMMUNITY<br><li>
+          <li><i class="fas fa-star mr-1"></i> <a href="https://nyzo.community" target="_blank">[HOT] Community forum</a></li>
+          <li><i class="fas fa-star mr-1"></i> <a href="https://www.reddit.com/r/Nyzo/comments/e4wizb/what_is_nyzo/" target="_blank"><i>[NEW] Introduction to Nyzo</i></a></li>
+          <li><i class="fas fa-star mr-1"></i> <a href="https://www.reddit.com/r/Nyzo/" target="_blank"><i>[NEW] Nyzo reddit</i></a></li>
+          <li><i class="fas fa-star mr-1"></i> <a href="https://www.facebook.com/nyzoblockchain/" target="_blank"><i>[NEW] Nyzo facebook</i></a></li>
+          <li><i class="fas fa-star mr-1"></i> <a href="https://www.zedge.net/profile/NYZO_currency" target="_blank"><i>[NEW] Nyzo on ZEDGE.net</i></a></li>
+          <li><i class="fab fa-discord mr-1"></i> <a href="https://nyzo.ondiscord.net" target="_blank">Discord</a></li>
+          <li><i class="fab fa-telegram mr-1"></i> <a href="https://t.me/NyzoCo" target="_blank">EN Telegram</a></li>
+          <li><i class="fab fa-telegram mr-1"></i> <a href="https://t.me/nyzo_cn" target="_blank">CN Telegram</a></li>
+        <li><i class="fas fa-file-pdf mr-1"></i> <a href="nyzo_01.pdf" target="_blank">NL bluepaper</a></li>
+        <li><i class="fas fa-link mr-1"></i> <a href="https://nyzo.me" target="_blank">Nyzo.me - Chinese community</a></li>
+        <li><i class="fas fa-link mr-1"></i> <a href="https://nyzo.io" target="_blank">Nyzo.io - English information</a></li>
+        <li><i class="fab fa-bitcoin mr-1"></i> <a href="https://bitcointalk.org/index.php?topic=5079696.0" target="_blank">Bitcointalk</a></li>
+        <li><i class="fab fa-twitter mr-1"></i> <a href="https://twitter.com/nyzo_currency" target="_blank">@nyzo_currency - Twitter</a></li>
+        <li><i class="fab fa-twitter mr-1"></i> <a href="https://twitter.com/nyzo_io" target="_blank"><i>[NEW] @nyzo_io - Twitter</i></a></li>
+        <li><i class="fab fa-twitter mr-1"></i> <a href="https://twitter.com/nyzogang" target="_blank"><i>[NEW] @nyzogang - Twitter</a></i></li>
+        <li><i class="fas fa-tshirt mr-1"></i> <a href="https://twitter.com/nyzoshop" target="_blank">@nyzoshop - Merch</a></li>
+        <li><i class="fab fa-qq mr-1"></i> QQ Group: 1007477423</li>
+        <li><hr>EXCHANGES<br><li>
+          <li><i class="fas fa-star mr-1"></i> <a href="https://buy.nyzo.net" target="_blank">Qtrade</a></li>
+          <li><i class="fas fa-hand-holding-usd mr-1"></i> <a href="https://citex.zendesk.com/hc/en-us/articles/360027202291-NYZO-will-be-launched-in-CITEX-P-Board-Trade-Zone-with-200-000CTT-Offered" target="_blank">Citex</a></li>
+          <li><i class="fas fa-hand-holding-usd mr-1"></i> <a href="https://medium.com/vinexnetwork/nyzo-is-now-listed-on-vinex-a0e3c57b1a2" target="_blank">Vinex</a></li>
+          <li><i class="fas fa-hand-holding-usd mr-1"></i> <a href="https://bilaxy.com/exchange#symbol=280" target="_blank">Bilaxy</a></li>
+          <li><i class="fas fa-hand-holding-usd mr-1"></i> <a href="https://bihodl.com/#/exchange/NYZO_USDT" target="_blank">Bihodl</a></li>
+        <li><hr>CONTENT<br><li>
+        <li><i class="fas fa-search mr-1"></i> <a href="https://myaltcoins.info/nyzo-review/" target="_blank">MyAltcoins.info - Nyzo review</a></li>
+        <li><i class="fas fa-microphone mr-1"></i> <a href="https://medium.com/@flashygordy/teamtalks-19-nyzo-aa4a24748aa2" target="_blank">Teamtalks - Nyzo dev team interview</a></li>
+        <li><i class="fab fa-medium mr-1"></i> <a href="https://medium.com/@nyzoco/the-nyzo-mesh-time-and-diversity-as-a-currency-85c676631516" target="_blank">Time and diversity as a currency</a></li>
+        <li><i class="fab fa-medium mr-1"></i> <a href="https://medium.com/@nyzoco/nyzo-addressing-arguments-against-proof-of-diversity-and-early-entrants-fa7cd4822012" target="_blank">Addressing arguments against Proof of diversity and early entrants</a></li>
+        <li><i class="fas fa-parachute-box mr-1"></i> <a href="https://explorer.nyzo.net/client/json/supply.json" target="_blank">Supply & marketcap</a></li>
+        <li><i class="fas fa-balance-scale mr-1"></i> <a href="https://explorer.nyzo.net/client/richlist.html" target="_blank"><i>[NEW] Top 250 Rich list</a></i></li>
+        <li><i class="fas fa-cubes mr-1"></i> <a href="http://nyzo.chainstats.net" target="_blank">Chain stats</a></li>
+        <li><i class="fas fa-info mr-1"></i> <a href="https://github.com/n-y-z-o/nyzoVerifier/blob/master/README.md" target="_blank">How to run a verifier</a></li>
+        <li><i class="fas fa-info mr-1"></i> <a href="https://www.youtube.com/watch?v=HIF4RHoNx2U" target="_blank">Setting up a verifier and sentinel (youtube)</a></li>
+
+
+
+      </ul>
+      <script src="https://widgets.coingecko.com/coingecko-coin-ticker-widget.js"></script>
+      <coingecko-coin-ticker-widget  coin-id="nyzo" currency="usd" locale="en"></coingecko-coin-ticker-widget>
+<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currencyid="5155" data-base="USD"></div>
+    </main>
+
+    <!-- <footer class="footer">
+      <div class="container">
+        <span class="text-muted">NYZO.NET</span>
+      </div>
+    </footer> -->
+  </body>
+</html>
